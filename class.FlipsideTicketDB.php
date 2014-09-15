621606f81a95d3037d75bc11e1d4b2f97a4a6744
Problem <pboyd04@gmail.com>
2014-09-15 18:34:55 +0000

    Add actual DB functionality and placeholder methods to DBAL

--- class.FlipsideTicketDB.php
+++ class.FlipsideTicketDB.php
@@ -5,6 +5,14 @@ require_once('class.FlipsideDonationType.php');
 require_once('class.FlipsideTicketRequest.php');
 class FlipsideTicketDB extends FlipsideDB
 {
+    protected static $test_mode = null;
+    protected static $year = null;
+
+    function __construct()
+    {
+        parent::__construct('tickets');
+    }
+
     function getRequestIdForUser($uesr)
     {
         return FALSE;
@@ -20,78 +28,93 @@ class FlipsideTicketDB extends FlipsideDB
         return FALSE;
     }
 
+    function getRequestCount()
+    {
+        return 0;
+    }
+
+    function getTicketSoldCount()
+    {
+        return 2000;
+    }
+
+    function getTicketUnsoldCount()
+    {
+        return 1000;
+    }
+
     function getFlipsideTicketConstraints()
     {
         $ret = new FlipsideTicketConstraints();
-        //TODO - OPbtain from DB
-        $ret->max_total_tickets = 4;
-        $ret->ticket_types = array();
-        $type = FlipsideTicketDB::getTicketTypeByType('A');
-        array_push($ret->ticket_types, $type);
-        $type = new FlipsideTicketType();
-        $type->typeCode = 'T';
-        $type->description = 'Teen (14 - 17)';
-        $type->cost = 95.00;
-        $type->max_per_request = 2;
-        array_push($ret->ticket_types, $type);
-        $type = new FlipsideTicketType();
-        $type->typeCode = 'K';
-        $type->description = 'Kid (7 - 13)';
-        $type->cost = 35.00;
-        $type->max_per_request = 2;
-        array_push($ret->ticket_types, $type);
-        $type = new FlipsideTicketType();
-        $type->typeCode = 'C';
-        $type->description = 'Child (0 - 6)';
-        $type->cost = 0.00;
-        $type->max_per_request = 2;
-        array_push($ret->ticket_types, $type);
+        $ret->max_total_tickets = $this->getVariable('max_tickets_per_request');
+        $ret->ticket_types = FlipsideTicketType::get_all_of_type($this);
         return $ret; 
     }
 
     function getFlipsideDonationTypes()
     {
-        //TODO - Get this from the SQL database
-        $ret = array();
-        $type = new FlipsideDonationType("Tinderbox");
-        $type->entityName = "Tinderbox";
-        $type->thirdParty = 1;
-        $type->url = 'http://tinderbox.es';
-        array_push($ret, $type);
-        return $ret;
+        return FlipsideDonationType::get_all_of_type($this);
+    }
+
+    function getVariable($name)
+    {
+        $array = $this->select('tblVariables', 'value', array('name'=>'=\''.$name.'\''));
+        if($array == FALSE || !isset($array[0]))
+        {
+            return FALSE;
+        }
+        switch($name)
+        {
+            case 'year':
+                self::$year = $array[0]['value'];
+                break;
+            case 'test_mode':
+                self::$test_mode = $array[0]['value'];
+                break;
+        }
+        return $array[0]['value'];
+    }
+
+    function getAllVars()
+    {
+        return $this->select('tblVariables');
+    }
+
+    function setVariable($name, $value)
+    {
+        $array = array('name'=>$name, 'value'=>$value);
+        return $this->replace_array('tblVariables', $array);
+    }
+
+    function deleteVariable($name)
+    {
+        return $this->delete('tblVariables', array('name'=>'=\''.$name.'\''));
     }
 
     static function getTicketTypeByType($type)
     {
-        $ret = new FlipsideTicketType();
-        switch($type)
+        $db = new static();
+        return FlipsideTicketType::select_from_db($db, 'typeCode', $type);
+    }
+
+    static function getTicketYear()
+    {
+        if(self::$year != null)
         {
-            case 'A':
-                $ret->typeCode = 'A';
-                $ret->description = 'Adult';
-                $ret->cost = 95.00;
-                $ret->max_per_request = 2;
-                break;
-            case 'T':
-                $ret->typeCode = 'A';
-                $ret->description = 'Teen (14 - 17)';
-                $ret->cost = 95.00;
-                $ret->max_per_request = 2;
-                break;
-            case 'K':
-                $ret->typeCode = 'A';
-                $ret->description = 'Kid (7 - 13)';
-                $ret->cost = 35.00;
-                $ret->max_per_request = 2;
-                break;
-            case 'C':
-                $ret->typeCode = 'A';
-                $ret->description = 'Child (0 - 6)';
-                $ret->cost = 0.00;
-                $ret->max_per_request = 2;
-                break;
+            return self::$year;
         }
-        return $ret;
+        $db = new static();
+        return $db->getVariable('year');
+    }
+
+    static function getTestMode()
+    {
+        if(self::$test_mode != null)
+        {
+            return self::$test_mode;
+        }
+        $db = new static();
+        return $db->getVariable('test_mode');
     }
 }
 ?>
