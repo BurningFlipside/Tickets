079ee5263d32a07dd404226a88f8fda8c3fd8b34
Problem <pboyd04@gmail.com>
2014-09-15 18:35:51 +0000

    Alert user if Ticket Window is closed

---
+++ ajax/window.php
@@ -0,0 +1,41 @@
+<?php
+if($_SERVER["HTTPS"] != "on")
+{
+    header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
+    exit();
+}
+require_once("class.FlipsideTicketDB.php");
+if(strtoupper($_SERVER['REQUEST_METHOD']) == 'GET')
+{
+    $db = new FlipsideTicketDB();
+    $vars = $db->getAllVars();
+    if($vars === FALSE)
+    {
+        echo json_encode(array('error' => "Internal Error! Failed to obtain variables!"));
+    }
+    else
+    {
+        $window = array();
+        for($i = 0; $i < count($vars); $i++)
+        {
+            switch($vars[$i]['name'])
+            {
+                case 'request_start_date':
+                case 'request_stop_date':
+                case 'mail_start_date':
+                case 'test_mode':
+                    $window[$vars[$i]['name']] = $vars[$i]['value'];
+                    break;
+            }
+        }
+        unset($vars);
+        echo json_encode(array('success' => 0, 'window' => $window));
+    }
+}
+else
+{
+    echo json_encode(array('error' => "Unrecognized Operation ".$_SERVER['REQUEST_METHOD']));
+    die();
+}
+/* vim: set tabstop=4 shiftwidth=4 expandtab: */
+?>
--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 function tableDrawComplete()
 {
-    if($("#ticketList").DataTable().data().length == 0)
+    if($("#ticketList").DataTable().data().length != 0)
     {
-        $("#ticket_set").hide();
+        $("#ticket_set").show();
     }
 }
 
@@ -46,8 +46,53 @@ function init_request()
         success: get_requests_done});
 }
 
+function get_window_done(data)
+{
+    if(data.window != undefined)
+    {
+        var my_window = data.window;
+        var now = Date.now();
+        var start = new Date(my_window.request_start_date);
+        var end = new Date(my_window.request_stop_date);
+        var mail_start = new Date(my_window.mail_start_date);
+        if(now < start || now > end)
+        {
+            var alert_div = $('<div/>', {class: 'alert alert-info', role: 'alert'});
+            $('<strong/>').html('Notice: ').appendTo(alert_div);
+            alert_div.append('The request window is currently closed.');
+            if(my_window.test_mode == '1')
+            {
+                alert_div.append(' But test mode is enabled. Any requests created will be deleted before ticketing starts!');
+                $('#request_set').prepend(alert_div);
+            }
+            else
+            {
+                $('#request_set').replaceWith(alert_div);
+            }
+        }
+        if(now > mail_start && now < end)
+        {
+            var days = Math.floor(end/(1000*60*60*24) - now/(1000*60*60*24));
+            var alert_div = $('<div/>', {class: 'alert alert-warning', role: 'alert'});
+            $('<strong/>').html('Notice: ').appendTo(alert_div);
+            alert_div.append('The mail in window is currently open! You have '+days+' days left to mail your request!');
+            $('#request_set').prepend(alert_div);
+        }
+    }
+}
+
+function init_window()
+{
+    $.ajax({
+        url: '/tickets/ajax/window.php',
+        type: 'get',
+        dataType: 'json',
+        success: get_window_done});
+}
+
 function init_index()
 {
+    init_window();
     init_request();
     init_table();
 }
