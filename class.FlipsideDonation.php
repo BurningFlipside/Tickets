68b12254f9a6d42243b4b1101cbcbc1d55e6f7cf
Problem <pboyd04@gmail.com>
2014-09-18 23:09:46 +0000

    Allow support for storing FlipsideDonations in the DB

--- class.FlipsideDonation.php
+++ class.FlipsideDonation.php
@@ -2,13 +2,34 @@
 require_once('class.FlipsideDonationType.php');
 class FlipsideDonation
 {
+    public $donation_id;
+    public $request_id;
+    public $year;
     public $type;
     public $amount;
+    public $disclose;
 
-    function __construct($type, $amount)
+    function __construct($type, $data)
     {
-        $this->type   = new FlipsideDonationType($type);
-        $this->amount = $amount;
+        $this->type       = new FlipsideDonationType($type);
+        $this->request_id = $data['request_id'];
+        $this->year       = $data['year'];
+        $this->amount     = $data['amount'];
+        if(isset($data['disclose']))
+        {
+            if($data['disclose'] == 'on')
+            {
+                $this->disclose = TRUE;
+            }
+            else
+            {
+                $this->disclose = $data['disclose'];
+            }
+        }
+        else
+        {
+            $this->disclose = FALSE;
+        }
     }
 }
 ?>
