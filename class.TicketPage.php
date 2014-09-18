9adeba930b83c94dda916751b8240503990f08f2
Problem <pboyd04@gmail.com>
2014-09-18 23:12:01 +0000

    Correct link to 'About AAR'

--- class.TicketPage.php
+++ class.TicketPage.php
@@ -66,7 +66,7 @@ class TicketPage extends SecurePage
         }
         $about_menu = array(
             'Burning Flipside'=>'http://www.burningflipside.com/about/event',
-            'AAR, LLC'=>'http://www.burningflipside.com/about/aar',
+            'AAR, LLC'=>'http://www.burningflipside.com/LLC',
             'Privacy Policy'=>'http://www.burningflipside.com/about/privacy'
         );
         $this->add_link('About', 'http://www.burningflipside.com/about', $about_menu);
