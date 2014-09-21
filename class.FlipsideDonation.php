2aa4f62b54c2930bb48ca636387c9aa33b5f7284
Problem <pboyd04@gmail.com>
2014-09-21 21:21:02 +0000

    Allow donation storage in DB

--- class.FlipsideDonation.php
+++ class.FlipsideDonation.php
@@ -1,13 +1,86 @@
 <?php
 require_once('class.FlipsideDonationType.php');
-class FlipsideDonation
+class FlipsideDonation extends FlipsideDBObject
 {
-    public $donation_id;
+    protected $_tbl_name = 'tblRequestDonation';
+    protected $_sql_special = array('type' => 'FlipsideDonation::type_to_entity_name');
+    protected $_sql_ai_key = array('donation_id');
+
+    public $donation_id = null;
     public $request_id;
     public $year;
     public $type;
     public $amount;
     public $disclose;
+
+    static function populate_children($db, &$type)
+    {
+        $type->type = FlipsideDonationType::select_from_db($db, 'entityName', $type->type);
+    }
+
+    static function select_from_db($db, $col, $value)
+    {
+        $type = parent::select_from_db($db, $col, $value);
+        if(is_array($type))
+        {
+            for($i = 0; $i < count($type); $i++)
+            {
+                self::populate_children($db, $type[$i]);
+            }
+        }
+        else
+        {
+            self::populate_children($db, $type);
+        }
+        return $type;
+    }
+
+    static function select_from_db_multi_conditions($db, $conds)
+    {
+        $type = parent::select_from_db_multi_conditions($db, $conds);
+        if(is_array($type))
+        {
+            for($i = 0; $i < count($type); $i++)
+            {
+                self::populate_children($db, $type[$i]);
+            }
+        }
+        else
+        {
+            self::populate_children($db, $type);
+        }
+        return $type;
+    }
+
+    protected function set_in_db($db, $op)
+    {
+        if($this->donation_id == null)
+        {
+            //Make sure I don't already have a ticket with the same request_id, year, and name
+            $res = $db->select($this->_tbl_name, 'donation_id', array('request_id'=>'=\''.$this->request_id.'\'', 'year'=>'=\''.$this->year.'\'', 'type'=>'=\''.$this->type->entityName.'\''));
+            if(count($res) > 0)
+            {
+                $this->donation_id = $res[0]['donation_id'];
+                $op = 'replace';
+            }
+        }
+        if($op == 'insert' && ($this->amount == 0 || $this->amount == null))
+        {
+            return 0;
+        }
+        else if($op == 'replace' && ($this->amount == 0 || $this->amount == null))
+        {
+            if($this->donation_id != null)
+            {
+                return $db->delete($this->_tbl_name, array('donation_id' => '=\''.$this->donation_id.'\''));
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        return parent::set_in_db($db, $op);
+    }
 
     function __construct($type, $data)
     {
@@ -30,6 +103,11 @@ class FlipsideDonation
         {
             $this->disclose = FALSE;
         }
+    }
+
+    static function type_to_entity_name($type)
+    {
+        return $type->entityName;
     }
 }
 ?>
