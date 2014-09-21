8474a34acd1e3b02511ea265b622dedf019a7bb0
Problem <pboyd04@gmail.com>
2014-09-21 21:17:27 +0000

    Convert request ajax to FlipJax

--- ajax/request.php
+++ ajax/request.php
@@ -1,138 +1,200 @@
 <?php
-if($_SERVER["HTTPS"] != "on")
-{
-    header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
-    exit();
-}
-require_once("class.FlipSession.php");
 require_once("class.FlipsideTicketDB.php");
 require_once("class.FlipsideTicketRequest.php");
-$user = FlipSession::get_user(TRUE);
-if($user == FALSE)
+require_once("class.FlipJax.php");
+class RequestAjax extends FlipJaxSecure
 {
-    echo json_encode(array('error' => "Not Logged In!"));
-    die();
-}
-$is_admin = $user->isInGroupNamed("TicketAdmins");
-$is_data  = $user->isInGroupNamed("TicketTeam");
-
-function get_single_value_from_array($array)
-{
-    if(!is_array($array))
+    function get_count()
     {
-        return $array;
-    }
-    if(isset($array[0]))
-    {
-        return $array[0];
-    }
-    else
-    {
-        return '';
-    }
-}
-
-if(strtoupper($_SERVER['REQUEST_METHOD']) == 'GET')
-{
-    if(isset($_GET['count']))
-    {
-        if(!$is_admin && !$is_data)
+        if(!$this->user_in_group("TicketAdmins") && !$this->user_in_group("TicketTeam"))
         {
-            echo json_encode(array('error' => "Access Denied! User must be a member of TicketAdmins or TicketTeam!"));
+            return array('err_code' => self::ACCESS_DENIED, 'reason' => "User must be a member of TicketAdmins or TicketTeam!");
+        }
+        $db = new FlipsideTicketDB();
+        $count = $db->getRequestCount();
+        if($count === FALSE)
+        {
+            return array('err_code' => self::INTERNAL_ERROR, 'reason' => "Failed to obtain request count!");
         }
         else
         {
-            $db = new FlipsideTicketDB();
-            $count = $db->getRequestCount();
-            if($count === FALSE)
+            return array('count' => $count);
+        }
+    }
+
+    function validate_user_can_read_id($id)
+    {
+        if($this->user_in_group("TicketAdmins") || $this->user_in_group("TicketTeam"))
+        {
+            return self::SUCCESS;
+        }
+        $my_id = FlipsideTicketRequest::getRequestId($this->user);
+        if($my_id == FALSE)
+        {
+            return array('err_code' => self::INTERNAL_ERROR, 'reason' => "Failed to obtain request ID for user!");
+        }
+        if($my_id != $id)
+        {
+            return array('err_code' => self::ACCESS_DENIED, 'reason' => "User must be a member of TicketAdmins or TicketTeam to access someone else's request!");
+        }
+        return self::SUCCESS;
+    }
+
+    function get_specific_request($id, $year)
+    {
+        $res = $this->validate_user_can_read_id($id);
+        if($res != self::SUCCESS)
+        {
+            return $res;
+        }
+        $db = new FlipsideTicketDB();
+        $requests = FlipsideTicketRequest::select_from_db($db, 'request_id', $id);
+        if($requests == FALSE)
+        {
+            //Still a success, they just have no requests
+            return self::SUCCESS;
+        }
+        else
+        {
+            if(!is_array($requests))
             {
-                echo json_encode(array('error' => "Internal Error! Failed to obtain request count!"));
+                $requests = array($requests);
+            }
+            if($year != null)
+            {
+                for($i = 0; $i < count($requests); $i++)
+                {
+                    if($requests[$i]->year == $year)
+                    {
+                        return array('requests' => array($requests[$i]));
+                    }
+                }
+                //Still a success, they just have no requests for this year
+                return self::SUCCESS;
             }
             else
             {
-                echo json_encode(array('success' => 0, 'count' => $count));
+                return array('requests' => $requests);
             }
         }
     }
-    else if(!isset($_GET['id']))
+
+    function get_request_id()
     {
         $id = FlipsideTicketRequest::getRequestId($user);
         if($id == FALSE)
         {
-            echo json_encode(array('error' => "Internal Error! Failed to obtain request ID for user!"));
+            return array('err_code' => self::INTERNAL_ERROR, 'reason' => "Failed to obtain request ID for user!");
+        }
+        return array('id' => $id);
+    }
+
+    function get($params)
+    {
+        if(!$this->is_logged_in())
+        {
+            return array('err_code' => self::ACCESS_DENIED, 'reason' => "Not Logged In!");
+        }
+        if(isset($params['count']))
+        {
+            return $this->get_count();
+        }
+        else if(isset($params['request_id']))
+        {
+            if(!isset($params['year']))
+            {
+                $params['year'] = null;
+            }
+            return $this->get_specific_request($params['request_id'], $params['year']);
+        }
+        else if(isset($params['full']))
+        {
+            if($this->user == null)
+            {
+                $this->user = FlipSession::get_user(TRUE);
+            }
+            $id = FlipsideTicketRequest::getRequestId($this->user);
+            if($id == FALSE)
+            {
+                return array('err_code' => self::INTERNAL_ERROR, 'reason' => "Failed to obtain request ID for user!");
+            }
+            return $this->get_specific_request($id, null);
         }
         else
         {
-            echo json_encode(array('success' => 0, 'id' => $id));
+            return $this->get_request_id();
         }
     }
-    else
+
+    function post_pdf($id, $year)
     {
-        $data = array();
-        echo json_encode(array('data'=>$data));
+        $res = $this->validate_user_can_read_id($id);
+        if($res != self::SUCCESS)
+        {
+            return $res;
+        }
+        $request = new FlipsideTicketRequest($id, FALSE, $year);
+        if($request == FALSE)
+        {
+            return array('err_code' => self::INTERNAL_ERROR, 'reason' => "Failed to obtain request!");
+        }
+        else
+        {
+            $file_name = $request->generatePDF();
+            if($file_name == FALSE)
+            {
+                return array('err_code' => self::INTERNAL_ERROR, 'reason' => "Failed to generate PDF!");
+            }
+            else
+            {
+                $file_name = substr($file_name, 1);
+                return array('pdf' => $file_name);
+            }
+        }
+    }
+
+    function post($params)
+    {
+        if(!$this->is_logged_in())
+        {
+            return array('err_code' => self::ACCESS_DENIED, 'reason' => "Not Logged In!");
+        }
+        if(isset($params['pdf']))
+        {
+            $res = $this->validate_params($params, array('request_id'=>'string', 'year'=>'int'));
+            if($res == self::SUCCESS)
+            {
+                $res = $this->post_pdf($params['request_id'], $params['year']);
+            }
+            return $res;
+        }
+        else
+        {
+            $res = $this->validate_params($params, array('request_id'=>'string', 'givenName'=>'string', 'sn'=>'string', 'mail'=>'string', 'mobile'=>'string',
+                                                         'c'=>'string', 'street'=>'string', 'zip'=>'string', 'l'=>'string', 'st'=>'string'));
+            if($res == self::SUCCESS)
+            {
+                $db = new FlipsideTicketDB();
+                $request = new FlipsideTicketRequest($params['request_id'], TRUE);
+                $request->populateFromPOSTData($params);
+                if(!isset($_POST['minor_confirm']) && $request->hasMinors())
+                {
+                    $res = array('need_minor_confirm' => '1');
+                }
+                else
+                {
+                    $request->modifiedBy = $user->uid[0];
+                    $request->modifiedByIP = $_SERVER['REMOTE_ADDR'];
+                    $request->replace_in_db($db);
+                    $res = self::SUCCESS;
+                }
+            }
+            return $res;
+        }
     }
 }
-else if(strtoupper($_SERVER['REQUEST_METHOD']) == 'POST')
-{
-    if(!isset($_POST['request_id']) || !is_string($_POST['request_id']))
-    {
-        echo json_encode(array('error' => "Invalid Parameter! Expected request_id as a string"));
-        die();
-    }
-    if(!isset($_POST['givenName']) || !is_string($_POST['givenName']))
-    {
-        echo json_encode(array('error' => "Invalid Parameter! Expected givenName as a string"));
-        die();
-    }
-    if(!isset($_POST['sn']) || !is_string($_POST['sn']))
-    {
-        echo json_encode(array('error' => "Invalid Parameter! Expected sn as a string"));
-        die();
-    }
-    if(!isset($_POST['mail']) || !is_string($_POST['mail']))
-    {
-        echo json_encode(array('error' => "Invalid Parameter! Expected mail as a string"));
-        die();
-    }
-    if(!isset($_POST['mobile']) || !is_string($_POST['mobile']))
-    {
-        echo json_encode(array('error' => "Invalid Parameter! Expected mobile as a string"));
-        die();
-    }
-    if(!isset($_POST['c']) || !is_string($_POST['c']))
-    {
-        echo json_encode(array('error' => "Invalid Parameter! Expected c as a string"));
-        die();
-    }
-    if(!isset($_POST['street']) || !is_string($_POST['street']))
-    {
-        echo json_encode(array('error' => "Invalid Parameter! Expected street as a string"));
-        die();
-    }
-    if(!isset($_POST['zip']) || !is_string($_POST['zip']))
-    {
-        echo json_encode(array('error' => "Invalid Parameter! Expected zip as a string"));
-        die();
-    }
-    if(!isset($_POST['l']) || !is_string($_POST['l']))
-    {
-        echo json_encode(array('error' => "Invalid Parameter! Expected l as a string"));
-        die();
-    }
-    if(!isset($_POST['st']) || !is_string($_POST['st']))
-    {
-        echo json_encode(array('error' => "Invalid Parameter! Expected st as a string"));
-        die();
-    }
-    $request = new FlipsideTicketRequest($_POST['request_id'], TRUE);
-    $request->populateFromPOSTData($_POST);
-    $request->generateBarcode();
-    echo json_encode($request);
-}
-else
-{
-    echo json_encode(array('error' => "Unrecognized Operation ".$_SERVER['REQUEST_METHOD']));
-    die();
-}
+
+$ajax = new RequestAjax();
+$ajax->run();
 /* vim: set tabstop=4 shiftwidth=4 expandtab: */
 ?>
