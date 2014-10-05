5098d5dd7d63c66475a89d3ccd8b9ddcd3b5bce1
Problem <pboyd04@gmail.com>
2014-10-05 22:40:50 +0000

    Port variable ajax to FlipJax

--- ajax/vars.php
+++ ajax/vars.php
@@ -1,85 +1,72 @@
 <?php
-if($_SERVER["HTTPS"] != "on")
-{
-    header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
-    exit();
-}
-require_once("class.FlipSession.php");
+require_once("class.FlipJax.php");
 require_once("class.FlipsideTicketDB.php");
-$user = FlipSession::get_user(TRUE);
-if($user == FALSE)
+class VariableAjax extends FlipJaxSecure
 {
-    echo json_encode(array('error' => "Not Logged In!"));
-    die();
-}
-$is_admin = $user->isInGroupNamed("TicketAdmins");
-
-if(strtoupper($_SERVER['REQUEST_METHOD']) == 'GET')
-{
-    if(!$is_admin)
+    function get($params)
     {
-        echo json_encode(array('error' => "Access Denied! User must be a member of TicketAdmins!"));
-    }
-    else
-    {
+        if(!$this->is_logged_in())
+        {
+            return array('err_code' => self::ACCESS_DENIED, 'reason' => "Not Logged In!");
+        }
+        if(!$this->user_in_group("TicketAdmins"))
+        {
+            return array('err_code' => self::ACCESS_DENIED, 'reason' => "User must be a member of TicketAdmins!");
+        }
         $db = new FlipsideTicketDB();
         $vars = $db->getAllVars();
         if($vars === FALSE)
         {
-            echo json_encode(array('error' => "Internal Error! Failed to obtain variables!"));
+            return array('err_code' => self::INTERNAL_ERROR, 'reason' => "Failed to obtain variables!");
         }
         else
         {
-            echo json_encode(array('success' => 0, 'vars' => $vars));
+            return array('vars' => $vars);
         }
-    } 
-}
-else if(strtoupper($_SERVER['REQUEST_METHOD']) == 'POST')
-{
-    if(!$is_admin)
-    {
-        echo json_encode(array('error' => "Access Denied! User must be a member of TicketAdmins!"));
-        die();
     }
-    if(isset($_POST['delete']))
+
+    function post($params)
     {
-        $db = new FlipsideTicketDB();
-        if($db->deleteVariable($_POST['delete']) === FALSE)
+        if(!$this->is_logged_in())
         {
-            echo json_encode(array('error' => "Internal Error! Failed to delete variable!"));
+            return array('err_code' => self::ACCESS_DENIED, 'reason' => "Not Logged In!");
+        }
+        if(!$this->user_in_group("TicketAdmins"))
+        {
+            return array('err_code' => self::ACCESS_DENIED, 'reason' => "User must be a member of TicketAdmins!");
+        }
+        if(isset($params['delete']))
+        {
+            $db = new FlipsideTicketDB();
+            if($db->deleteVariable($_POST['delete']) === FALSE)
+            {
+                return array('err_code' => self::INTERNAL_ERROR, 'reason' => "Failed to delete variable!");
+            }
+            else
+            {
+                return self::SUCCESS;
+            }
         }
         else
         {
-            echo json_encode(array('success' => 0));
-        }
-    }
-    else
-    {
-        if(!isset($_POST['name']) || !is_string($_POST['name']))
-        {
-            echo json_encode(array('error' => "Invalid Parameter! Expected name as a string"));
-            die();
-        }
-        if(!isset($_POST['value']) || !is_string($_POST['value']))
-        {
-            echo json_encode(array('error' => "Invalid Parameter! Expected value as a string"));
-            die();
-        }
-        $db = new FlipsideTicketDB();
-        if($db->setVariable($_POST['name'], $_POST['value']) === FALSE)
-        {
-            echo json_encode(array('error' => "Internal Error! Failed to set variable!"));
-        }
-        else
-        {
-            echo json_encode(array('success' => 0));
+            $res = $this->validate_params($params, array('name'=>'string', 'value'=>'string'));
+            if($res == self::SUCCESS)
+            {
+                if($db->setVariable($_POST['name'], $_POST['value']) === FALSE)
+                {
+                    $res = array('err_code' => self::INTERNAL_ERROR, 'reason' => "Failed to set variable!");
+                }
+                else
+                {
+                    $res = self::SUCCESS;
+                }
+            }
+            return $res;
         }
     }
 }
-else
-{
-    echo json_encode(array('error' => "Unrecognized Operation ".$_SERVER['REQUEST_METHOD']));
-    die();
-}
+
+$ajax = new VariableAjax();
+$ajax->run();
 /* vim: set tabstop=4 shiftwidth=4 expandtab: */
 ?>
