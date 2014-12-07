4634fe6160130fe42b6219dce00ada58c855e18e
Problem <pboyd04@gmail.com>
2014-12-07 16:06:32 +0000

    Substitute variables in Ticket Emails

--- class.TicketEmail.php
+++ class.TicketEmail.php
@@ -20,19 +20,41 @@ class TicketEmail extends FlipsideMail
         $this->From     = 'tickets@burningflipside.com';
         $this->FromName = 'Flipside Tickets';
         $this->clearAllRecipients();
+        $barcode        = '<barcode code="'.$this->ticket->hash.'" type="C93"/>';
+        $transfer_url   = 'https://secure.burningflipside.com/tickets/transfer.php?id='.$this->ticket->hash;
+        $transfer_qr    = '<barcode code="'.$transfer_url.'" type="QR" class="barcode" size="1" error="M" />';
+        $year           = $this->ticket->year;
+        $ticket_id      = $this->ticket->hash;
+        $short_id       = substr($this->ticket->hash, 0, 8);
+        $word_code      = Ticket::hash_to_words($this->ticket->hash);
+        $name           = $this->ticket->firstName.' '.$this->ticket->lastName;
+        $email          = $this->ticket->email;
+        $type           = $this->ticket->type;
+        $vars           = array(
+            '{$barcode}'        => $barcode,
+            '{$transfer_url}'   => $transfer_url,
+            '{$transfer_qr}'    => $transfer_qr,
+            '{$year}'           => $year,
+            '{$ticket_id}'      => $ticket_id,
+            '{$short_id}'       => $short_id,
+            '{$word_code}'      => $word_code,
+            '{$name}'           => $name,
+            '{$email}'          => $email,
+            '{$type}'           => $type
+        );
         if($this->email == $ticket->email)
         {
             $this->addAddress($this->email, $this->ticket->firstName.' '.$this->ticket->lastName);
             $this->Subject = 'Burning Flipside '.$this->ticket->year.' Will Call Ticket Form';
-            $this->Body    = FlipsideTicketDB::get_long_text('ticket_email_source');
-            $this->AltBody = strip_tags(FlipsideTicketDB::get_long_text('ticket_email_source'));
+            $this->Body    = strtr(FlipsideTicketDB::get_long_text('ticket_email_source'), $vars);
+            $this->AltBody = strtr(strip_tags(FlipsideTicketDB::get_long_text('ticket_email_source')), $vars);
         }
         else
         {
             $this->addAddress($this->email);
             $this->Subject = 'Burning Flipside '.$this->ticket->year.' Will Call Ticket Transfer';
-            $this->Body    = FlipsideTicketDB::get_long_text('ticket_transfer_email_source');
-            $this->AltBody = strip_tags(FlipsideTicketDB::get_long_text('ticket_transfer_email_source'));
+            $this->Body    = strtr(FlipsideTicketDB::get_long_text('ticket_transfer_email_source'), $vars);
+            $this->AltBody = strtr(strip_tags(FlipsideTicketDB::get_long_text('ticket_transfer_email_source')), $vars);
         }
         $this->isHTML(true);
         if($this->attach_pdf)
