3670988befd3f9cc779254f61336c71d5a73e4c4
Problem <pboyd04@gmail.com>
2015-01-27 04:52:42 +0000

    Issue 11: Make tables printable

--- _admin/js/problems.js
+++ _admin/js/problems.js
@@ -13,9 +13,45 @@ function init_table()
     });
 }
 
+function expand_table()
+{
+    $(this).DataTable().page.len(-1);
+}
+
+function before_print()
+{
+    $('table').each(expand_table);
+}
+
+function after_print()
+{
+    console.log('after');
+}
+
+function on_print_change(mql)
+{
+    if(mql.matches)
+    {
+        before_print();
+    }
+    else
+    {
+        after_print();
+    }
+}
+
 function init_page()
 {
     $('table').each(init_table);
+    if(window.matchMedia !== undefined)
+    {
+        //WebKit implementation
+        var mediaQueryList = window.matchMedia('print');
+        mediaQueryList.addListener(on_print_change);
+    }
+    //IE & Firefox implementation
+    window.onbeforeprint = before_print;
+    window.onafterprint = after_print;
 }
 
 $(init_page);
