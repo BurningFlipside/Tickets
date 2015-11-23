08974bdd91b77ee1daaeb646125265cbb663f35d
Problem <pboyd04@gmail.com>
2015-11-23 02:46:21 +0000

    Switch TicketAdminPage to new DataTable API

--- _admin/index.php
+++ _admin/index.php
@@ -2,12 +2,14 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 require_once('class.TicketAdminPage.php');
-require_once('class.FlipsideTicketDB.php');
 $page = new TicketAdminPage('Burning Flipside - Tickets');
 
 $page->add_js_from_src('js/index.js');
 
-$db = new FlipsideTicketDB();
+$data_set = DataSetFactory::get_data_set('tickets');
+$data_table = $data_set['Problems'];
+$settings = \Tickets\DB\TicketSystemSettings::getInstance();
+$year = $settings['year'];
 
 $page->body .= '
 <div class="row">
@@ -19,7 +21,7 @@ $page->body .= '
 
 $page->add_card('fa-file', '<div id="requestCount">?</div>', 'Ticket Requests', 'requests.php');
 $page->add_card('fa-tag',  '<div id="requestedTicketCount">?</div>', 'Requested Tickets', 'request_tickets.php', $page::CARD_GREEN);
-$page->add_card('fa-fire', $db->getProblemRequestCount(), 'Problem Requests', 'problems.php', $page::CARD_RED);
+$page->add_card('fa-fire', $data_table->count(new \Data\Filter('year eq '.$year)), 'Problem Requests', 'problems.php', $page::CARD_RED);
 $page->add_card('fa-usd',  '<div id="soldTicketCount">?</div>', 'Sold Tickets', 'sold_tickets.php', $page::CARD_YELLOW);
 $page->body.='</div>';
 
