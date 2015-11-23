00a4a84a9dc136bf6aa0c8d09405d5068ea3d529
Problem <pboyd04@gmail.com>
2015-11-23 22:13:55 +0000

    Fix no login verify page

--- verify.php
+++ verify.php
@@ -2,7 +2,6 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 require_once('class.TicketPage.php');
-require_once('class.Ticket.php');
 $page = new TicketPage('Burning Flipside - Tickets');
 
 $page->add_js_from_src('js/verify.js');
@@ -11,14 +10,7 @@ if(!FlipSession::isLoggedIn())
 {
     $page->body .= '
 <div id="content">
-    <h1>Login</h1>
-    <form id="login_form" role="form" action="https://profiles.burningflipside.com/ajax/login.php" method="POST">
-        <input class="form-control" type="text" name="username" placeholder="Username or Email" required autofocus/>
-        <input class="form-control" type="password" name="password" placeholder="Password" required/>
-        <input type="hidden" name="return" value="'.$page->current_url().'"/>
-        <input type="hidden" name="redirect" value="1"/>
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>
-    </form>
+    <h1>You must <a data-toggle="modal" data-target="#login-dialog" style="cursor: pointer;">log in <span class="glyphicon glyphicon-log-in"></span></a> to access the Burning Flipside Ticket system!</h1>
 </div>';
 }
 else
