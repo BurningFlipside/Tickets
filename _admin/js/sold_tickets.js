59dbfa65c5cd71fa713ee2ca2149593f3252fd1c
Problem <pboyd04@gmail.com>
2015-11-24 20:45:01 +0000

    Remove deprecated API call from sold tickets admin page

--- _admin/js/sold_tickets.js
+++ _admin/js/sold_tickets.js
@@ -1,12 +1,12 @@
 function short_hash(data, type, row, meta)
 {
-    return data.substring(0,7);
+    return data.substring(0,8);
 }
 
 function init_page()
 {
     $('#tickets').dataTable({
-        "ajax": '/tickets/ajax/tickets.php?all=1&meta=sold',
+        "ajax": '../api/v1/tickets?filter=sold eq 1 and year eq 2016',
         columns: [
             {'data': 'hash', 'render':short_hash},
             {'data': 'firstName'},
