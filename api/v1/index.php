1367489b65c7390e7e373a3174c23cfb65ee7fdd
Problem <pboyd04@gmail.com>
2016-02-10 21:31:53 +0000

    Make Ticket API discoverable

--- api/v1/index.php
+++ api/v1/index.php
@@ -12,6 +12,9 @@ if($_SERVER['REQUEST_URI'][0] == '/' && $_SERVER['REQUEST_URI'][1] == '/')
 }
 
 $app = new FlipREST();
+$app->get('(/)', 'getRoot');
+$app->get('$metadata', 'getMetadata');
+$app->get('/$metadata', 'getMetadata');
 $app->group('/ticket', 'ticket_api_group');
 $app->group('/tickets', 'ticket_api_group');
 $app->group('/tickets_history', 'ticket_history_api_group');
@@ -19,6 +22,26 @@ $app->group('/request', 'request_api_group');
 $app->group('/requests', 'request_api_group');
 $app->group('/requests_w_tickets', 'request_ticket_api_group');
 $app->group('/globals', 'global_api_group');
+
+function getRoot()
+{
+    global $app;
+    $ret = array();
+    $root = $app->request->getRootUri();
+    $ret['@odata.context'] = $root.'/$metadata';
+    $ret['value'] = array();
+    $ret['value']['Tickets'] = array('@odata.id'=>$root.'/tickets');
+    $ret['value']['TicketsHistory'] = array('@odata.id'=>$root.'/tickets_history');
+    $ret['value']['Requests'] = array('@odata.id'=>$root.'/requests');
+    $ret['value']['Globals'] = array('@odata.id'=>$root.'/globals');
+    echo json_encode($ret);
+}
+
+function getMetadata()
+{
+    global $app;
+    echo file_get_contents('csdl.xml');
+}
 
 $app->run();
 ?>
