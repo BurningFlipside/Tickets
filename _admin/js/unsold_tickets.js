404c4fccd3d32f4693a3e9c2bd87fb1896f3d7af
Problem <pboyd04@gmail.com>
2016-04-25 00:26:46 +0000

    Fix unsold ticket table

--- _admin/js/unsold_tickets.js
+++ _admin/js/unsold_tickets.js
@@ -1,7 +1,7 @@
 function init_page()
 {
     $('#tickets').dataTable({
-        "ajax": '../api/v1/tickets?filter=sold eq 0&fmt=data-table',
+        "ajax": '../api/v1/tickets?filter=sold eq 0 and used eq 0 and year eq 2016&fmt=data-table',
         columns: [
             {'data': 'hash'},
             {'data': 'type'}
