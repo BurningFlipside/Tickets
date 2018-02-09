5b8773fcdc56a1a945cd926db5988f5c450b224a
Problem <pboyd04@gmail.com>
2018-02-09 17:04:18 +0000

    Fix typos in TicketAPI

--- api/v1/class.TicketAPI.php
+++ api/v1/class.TicketAPI.php
@@ -362,7 +362,7 @@ class TicketAPI extends Http\Rest\RestAPI
         return $response->withJson($res); 
     }
 
-    public function verifyShortCode($request, $response, $app)
+    public function verifyShortCode($request, $response, $args)
     {
         $this->validateLoggedIn($request);
         $code = $args['code'];
@@ -379,7 +379,7 @@ class TicketAPI extends Http\Rest\RestAPI
         return $response->withJson($res);
     }
 
-    public function generateTickets($request, $response, $app)
+    public function generateTickets($request, $response, $args)
     {
         $this->validateLoggedIn($request);
         if(!$this->user->isInGroupNamed('TicketAdmins'))
