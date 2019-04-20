e449550170e826eea4e26abca382eedb359723d8
Problem <pboyd04@gmail.com>
2019-04-20 02:43:18 +0000

    Fix dialog close button on pools page

--- _admin/pools.php
+++ _admin/pools.php
@@ -29,8 +29,8 @@ $page->body .= '
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
                         <h4 class="modal-title" id="modal_title">Edit Pool #<span id="_id"></span></h4>
+                        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
                     </div>
                     <div class="modal-body">
                         <div class="form-group">
@@ -59,8 +59,8 @@ $page->body .= '
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
                         <h4 class="modal-title" id="modal_title">New Ticket Pool</h4>
+                        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
                     </div>
                     <div class="modal-body">
                         <div class="form-group">
