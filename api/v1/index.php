925fb3e4aef94f7e658c2cf8a52e1571e6730424
Problem <pboyd04@gmail.com>
2024-04-23 19:48:54 +0000

    Add pending sales and costs API endpoints

--- api/v1/index.php
+++ api/v1/index.php
@@ -22,16 +22,19 @@ require('class.SecondaryAPI.php');
 require('class.TicketAPI.php');
 require('class.TicketHistoryAPI.php');
 require('class.Square.php');
+require('class.PendingPurchaseAPI.php');
+require('class.EarlyEntryAPI.php');
 
 $site = new \Flipside\Http\WebSite();
 $site->registerAPI('/', new BaseAPI());
-$site->registerAPI('/earlyEntry', new AdminTicketDataTableAPI('tickets', 'EarlyEntryMap', 'earlyEntrySetting'));
+$site->registerAPI('/earlyEntry', new EarlyEntryAPI());
 $site->registerAPI('/globals', new GlobalAPI());
 $site->registerAPI('/globals/donation_types', new TicketDataTableAPI('tickets', 'DonationTypes', 'entityName'));
 $site->registerAPI('/globals/ticket_types', new TicketDataTableAPI('tickets', 'TicketTypes', 'TypeCode'));
 $site->registerAPI('/globals/statuses', new TicketDataTableAPI('tickets', 'RequestStatus', 'status_id'));
 $site->registerAPI('/globals/vars', new AdminTicketDataTableAPI('tickets', 'Variables', 'name'));
 $site->registerAPI('/globals/long_text', new AdminTicketDataTableAPI('tickets', 'LongText', 'name'));
+$site->registerAPI('/globals/costs', new AdminTicketDataTableAPI('tickets', 'CostHistory', 'year'));
 $site->registerAPI('/pools', new PoolAPI());
 $site->registerAPI('/requests', new RequestAPI());
 $site->registerAPI('/request', new RequestAPI());
@@ -41,5 +44,6 @@ $site->registerAPI('/square', new SquareAPI());
 $site->registerAPI('/ticket', new TicketAPI());
 $site->registerAPI('/tickets', new TicketAPI());
 $site->registerAPI('/tickets_history', new TicketHistoryAPI());
+$site->registerAPI('/pendingSales', new PendingPurchaseAPI());
 $site->run();
 /* vim: set tabstop=4 shiftwidth=4 expandtab: */
